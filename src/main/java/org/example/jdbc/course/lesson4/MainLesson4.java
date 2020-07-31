d8cbd11b7fc3c9f2a1d9bd30279bf96755cff52c
sergias <sergias@ukr.net>
2020-07-31 18:21:07 +0300

    ITVDN - JDBC, lessons update

--- src/main/java/org/example/jdbc/course/lesson4/MainLesson4.java
+++ src/main/java/org/example/jdbc/course/lesson4/MainLesson4.java
@@ -10,39 +10,48 @@ public class MainLesson4 {
 		     Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
 			ResultSet rs = null;
 			try {
+				// Получаем результирующий набор
 				rs = statement.executeQuery("SELECT * FROM books");
-//				while (rs.next()) {
-//					int id = rs.getInt(1);
-//					double price = rs.getDouble(3);
-//					if (id == 2) {
-//						rs.updateString("name", "Spartacus");
-//						rs.updateDouble(3, price - 10);
-//						rs.updateRow();
-//					}
-//				}
+				// Обновляем одну из записей в таблице Books
+//                while (rs.next()) {
+//                    int id = rs.getInt(1);
+//                    double price = rs.getDouble(3);
+//                    if(id == 4) {
+//                        rs.updateString("name" , "Spartacus (discount)");
+//                        rs.updateDouble(3, price - 10);
+//                        rs.updateRow();
+//                    }
+//                }
 
-				if (rs.absolute(3)) System.out.println(rs.getString("name"));
-				if (rs.previous()) System.out.println(rs.getString("name"));
-				if (rs.last()) System.out.println(rs.getString("name"));
-				if (rs.relative(-4)) {
+				// Переходим по результирующему набору с помощью различных методов
+				if (rs.absolute(2))
+					System.out.println(rs.getString("name"));
+				if (rs.previous())
+					System.out.println(rs.getString("name"));
+				if (rs.last())
+					System.out.println(rs.getString("name"));
+
+				// Передвигаем курсор на три строки назад
+				if (rs.relative(-3)) {
+					// Получаем объект метаданных результирующего набора
 					ResultSetMetaData rsmd = rs.getMetaData();
+					// Выводим строки результирующего набора с помощъю метаданных
 					while (rs.next()) {
-						for (int i = 1; i < rsmd.getColumnCount(); i++) {
+						for (int i = 1; i <= rsmd.getColumnCount(); ++i) {
 							String field = rsmd.getColumnName(i);
 							String value = rs.getString(field);
 							System.out.print(field + ": " + value + " ");
 						}
-						System.out.println();
+						System.out.println("");
 					}
 				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException exc) {
+				exc.printStackTrace();
 			} finally {
-				if (rs != null) {
+				if (rs != null)
 					rs.close();
-				} else {
-					System.out.println("ERROR!");
-				}
+				else
+					System.err.println("Ошибка чтения данных с БД!");
 			}
 		}
 	}
--- src/main/java/org/example/jdbc/course/lesson4/RowSetLesson.java
+++ src/main/java/org/example/jdbc/course/lesson4/RowSetLesson.java
@@ -8,56 +8,68 @@ import java.sql.*;
 import static org.example.jdbc.Jdbc.getNewConnection;
 
 public class RowSetLesson {
+	// Задаем параметры подключения как статические переменные
 	static String url = "jdbc:mysql://localhost:3306/study_tech";
 	static String userName = "root";
 	static String password = "root";
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//		ResultSet resultSet = getResSet();
-////		while (resultSet.next()) {
-////			System.out.println(resultSet.getString("name"));
-////		}
-//		CachedRowSet rowSet = (CachedRowSet) resultSet;
-//		rowSet.setCommand("SELECT * FROM books WHERE price > ?");
-//		rowSet.setDouble(1, 30);
-//		rowSet.setUrl(url);
-//		rowSet.setUsername(userName);
-//		rowSet.setPassword(password);
-//		rowSet.execute();
-//
-//		// перемещаемся на 2ую позицию набора строк, НЕ БД!!!
-//		rowSet.absolute(2);
-//		rowSet.deleteRow();
-//		rowSet.beforeFirst();
-//		Connection connection = getNewConnection();
-//		connection.setAutoCommit(false);
-//		rowSet.acceptChanges(connection);
-//
-//		while (rowSet.next()) {
-//			String name = rowSet.getString("name");
-//			double price = rowSet.getDouble(3);
-//			System.out.println(name + ": " + price);
-//		}
+		// Получаем результрующий набор от метода getResSet()
+		ResultSet resultSet = getResSet();
+//        while (resultSet.next())
+//            System.out.println(resultSet.getString("name"));
+
+		// Создаем новый объект кэшированного набора строк
+		CachedRowSet rowSet = (CachedRowSet) resultSet;
+		// Выполняем запрос к набору строк или к базе данных
+		rowSet.setCommand("SELECT * FROM books WHERE price > ?");
+		rowSet.setDouble(1, 30);
+
+		// Задаем параметры подключения к БД
+		rowSet.setUrl(url);
+		rowSet.setUsername(userName);
+		rowSet.setPassword(password);
+		rowSet.execute();
+
+		// Вносим изменения в набор строк и базу данных
+		rowSet.absolute(2);
+		rowSet.deleteRow();
+		rowSet.beforeFirst();
+		Connection connection = DriverManager.getConnection(url, userName, password);
+		connection.setAutoCommit(false);
+		rowSet.acceptChanges(connection);
+
+		// Выводим набор строк
+		while (rowSet.next()) {
+			String name = rowSet.getString("name");
+			double price = rowSet.getDouble(3);
+			System.out.println(name + " " + price);
+		}
 
 		insertNew("Terminator", 67.99);
 	}
 
+	// Метод для получения результирующего набора
 	static ResultSet getResSet() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		try(Connection connection = DriverManager.getConnection(url, userName, password);
-		    Statement statement = connection.createStatement()) {
-
-			ResultSet rs = statement.executeQuery("SELECT * FROM books");
+		// Регистрируем драйвер и выполняем подключение к БД
+		Class.forName("com.mysql.jdbc.Driver");
+		try (Connection conn = DriverManager.getConnection(url, userName, password);
+		     Statement stat = conn.createStatement()) {
+			// Получаем результирующий набор
+			ResultSet rs = stat.executeQuery("SELECT * FROM books");
+			// Получаем объект типа RowSetFactory
 			RowSetFactory factory = RowSetProvider.newFactory();
+			// Получаем объект кэшированного набора строк
 			CachedRowSet crs = factory.createCachedRowSet();
+			// Наполняем набор данными
 			crs.populate(rs);
 			return crs;
 		}
 	}
 
 	static void insertNew(String name, double price) throws SQLException {
-		try(Connection connection = getNewConnection();
-		    Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
+		try (Connection connection = getNewConnection();
+		     Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
 
 			ResultSet rs = null;
 			try {
